d3d173f39c79e420cef13b553c1b19dd0d58a685
Sebastien Windal <swindal@wgen.net>
2013-02-01 14:40:47 -0500

    stuff

--- public/javascripts/Draw.js
+++ public/javascripts/Draw.js
@@ -276,6 +276,7 @@ var canvas, stage;
         onDrawSegmentReceived(message.start.x, message.start.y, message.end.x, message.end.y, message.color, message.stroke, message.user);
         break;
         case 'clear':
+        alert(drawingCanvases[message.user]);
           drawingCanvases[message.user].graphics.clear();
         break;
       }
