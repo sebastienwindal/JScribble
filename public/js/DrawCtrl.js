4768ebe486cf95a03615d0af71c1b9447082a728
Sebastien Windal <swindal@wgen.net>
2013-03-13 22:41:27 -0400

    more progress

--- public/js/DrawCtrl.js
+++ public/js/DrawCtrl.js
@@ -12,15 +12,10 @@ app.directive('drawingBoard', function() {
 		link: function(scope, element, attrs) {
 
 			scope.handleMouseDown = function() {
-				debugger;
-				if (this.stage.contains(title)) { 
-	        		this.stage.clear(); 
-	        		this.stage.removeChild(title); 
-	    		}
 
-	    		oldPt = new createjs.Point(this.stage.mouseX, this.stage.mouseY);
+	    		oldPt = new createjs.Point(scope.stage.mouseX, scope.stage.mouseY);
 	    		oldMidPt = oldPt;
-	    		this.stage.addEventListener("stagemousemove" , scope.handleMouseMove);
+	    		scope.stage.addEventListener("stagemousemove" , scope.handleMouseMove);
 
 	    		//socket.emit('draw', $.toJSON({  action: 'start', 
 	            //                        user: clientName,
@@ -31,52 +26,58 @@ app.directive('drawingBoard', function() {
 			};
 
 			scope.handleMouseMove = function() {
-				debugger;
-				var midPt = new createjs.Point(oldPt.x + this.stage.mouseX>>1, oldPt.y+this.stage.mouseY>>1);
 
-	    		drawingCanvas.graphics.clear().setStrokeStyle(3 /* stroke */, 'round', 'round').beginStroke('#000000' /*penColor*/).moveTo(midPt.x, midPt.y).curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
+				var midPt = new createjs.Point(oldPt.x + scope.stage.mouseX>>1, oldPt.y+scope.stage.mouseY>>1);
 
-	    		oldPt.x = this.stage.mouseX;
-	    		oldPt.y = this.stage.mouseY;
+	    		drawingCanvas.graphics.clear()	.setStrokeStyle(5 /* stroke */, 'round', 'round')
+	    										.beginStroke('#000000' /*penColor*/)
+	    										.moveTo(midPt.x, midPt.y)
+	    										.curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
+
+	    		oldPt.x = scope.stage.mouseX;
+	    		oldPt.y = scope.stage.mouseY;
 
 	    		oldMidPt.x = midPt.x;
 	    		oldMidPt.y = midPt.y;
 
-	    		this.stage.update();
+	    		scope.stage.update();
 
 	    		//socket.emit('draw', $.toJSON({action: 'draw', user: clientName, start: oldMidPt, end:midPt, color:penColor, stroke: stroke }));
 			};
+
 			scope.handleMouseUp = function() {
-				debugger;
-				this.stage.removeEventListener("stagemousemove" , scope.handleMouseMove);
+				scope.stage.removeEventListener("stagemousemove" , scope.handleMouseMove);
 			};
+
+			scope.stageResize = function() { 
+ 		       	scope.stage.canvas.width = element.width();
+        		scope.stage.canvas.height = element.height();       
+    		}
 
 			var canvas = angular.element(element)[0];
 
+			$(window).resize(function() {
+			  	scope.stageResize();
+			});
 			//drawingCanvases[clientName] = canvas;
 
 		    //check to see if we are running in a browser with touch support
-		    this.stage = new createjs.Stage(canvas);
+		    scope.stage = new createjs.Stage(canvas);
 
-		    this.stage.autoClear = false;
-		    this.stage.enableEvents(true);
+		    scope.stage.autoClear = false;
+		    scope.stage.enableEvents(true);
 
-		    createjs.Touch.enable(this.stage);
+		    createjs.Touch.enable(scope.stage);
 		    createjs.Ticker.setFPS(24);
 
 		    var drawingCanvas = new createjs.Shape();
 		    //drawingCanvases[clientName] = drawingCanvas;
-debugger;
-		    this.stage.addEventListener("stagemousedown", scope.handleMouseDown);
-		    this.stage.addEventListener("stagemouseup", scope.handleMouseUp);
 
-		    title = new createjs.Text("Click and Drag to draw", "36px Arial", "#777777");
-		    title.x = 300;
-		    title.y = 200;
-		    this.stage.addChild(title);
+		    scope.stage.addEventListener("stagemousedown", scope.handleMouseDown);
+		    scope.stage.addEventListener("stagemouseup", scope.handleMouseUp);
 
-		    this.stage.addChild(drawingCanvas);
-		    this.stage.update();			
+		    scope.stage.addChild(drawingCanvas);
+		    scope.stage.update();			
 		}
 	}
 	return mydirective;
